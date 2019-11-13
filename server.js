f3f35f55e0f99c60b84fdc82aa619f0c0f526863
Jose Mulet Pol <josep@iMac-de-Jose.local>
2019-11-13 18:44:21 +0100

    local resources

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js
 // load the things we need
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require("fs");
+const app = express();
+
 app.use(bodyParser.text({ type: 'text/html', defaultCharset: "iso-8859-1" }));
 
 app.use((req, res, next) => { //change app.all to app.use here and remove '*', i.e. the first parameter part
@@ -17,8 +19,31 @@ app.use((req, res, next) => { //change app.all to app.use here and remove '*', i
   next();
 });
 
+
+app.use('/database/:schema', function (req, res, next) {
+  
+  const json = require('./database/'+req.params.schema);
+  res.send(JSON.stringify(json));
+});
+
+
+// dynamic routes
+app.use('/database/', function (req, res, next) {
+  // List all databases 
+  const dbFiles = fs.readdirSync("./database");
+  let html  = "<h4>Trieu la taula de dades</h4><ul>";
+  dbFiles.forEach(file => {
+    html += `<li><a href="/database/${file}">${file}</a></li>`
+  });
+  html +="</ul>";
+  res.send(html);
+});
+
 // static server 
 app.use('/', express.static('static'));
+
+
+
 
 app.listen(3000);
 console.log('Application server running on port 3000');
